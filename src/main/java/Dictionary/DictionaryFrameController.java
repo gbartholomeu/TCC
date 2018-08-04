fd4e7726dc49c8bf67d5d915eb424978d83cfeae
ronyjack <ronyjack@192.168.25.2>
2018-08-03 23:14:56 -0300

    System template adjustments

--- src/main/java/Dictionary/DictionaryFrameController.java
+++ src/main/java/Dictionary/DictionaryFrameController.java
@@ -13,17 +13,21 @@ import javax.swing.JTextArea;
  */
 public class DictionaryFrameController {
 
+    //SET GLOBAL log_bin_trust_function_creators = 1;
     public static String getProcedureFunctionTriggerTemplate(String nameObject, boolean isProcedure, boolean isFunction, boolean isTrigger) {
 
         String procFuncTrig = nameObject.equals("") ? "new_procedure" : nameObject;
         String type = (isProcedure) ? "PROCEDURE" : (isFunction) ? "FUNCTION" : (isTrigger) ? "TRIGGER" : "";
         //"DROP " + type + " IF EXISTS " + procFuncTrig + ";\n"
 
-        return "CREATE " + type + " `" + procFuncTrig + "` ()\n"
+        return "CREATE " + type + " `" + procFuncTrig + "`"
+                + (isFunction || isProcedure ? " ()\n" : "\n")
+                + (isFunction ? " RETURNS INT(255)\n" : (isTrigger ? " :CONDICAO ON :TABELA_ALVO \n" : "\n"))
+                + (isTrigger ? "FOR EACH ROW \n" : "\n")
                 + "BEGIN\n"
+                + (isFunction ? "DECLARE OBJECT_RETURN INT(255);\n" : "\n")
                 + "\n"
-                + "\n"
-                + "\n"
+                + (isFunction ? "RETURN OBJECT_RETURN;\n" : "\n")
                 + "\n"
                 + "END;";
     }
